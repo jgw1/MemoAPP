c0095d69a64e13d6f8d6e0f54c8a487352fae042
gwjung <gwjung@vdotdo.com>
2019-06-27 13:37:14 +0900

    Splash(Loading)-Main

--- app/src/main/java/com/practice/mission1/SplashActivity.java
+++ app/src/main/java/com/practice/mission1/SplashActivity.java
@@ -7,17 +7,18 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
-    private static int time = 1000;
+    private static int time = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
         new Handler().postDelayed(new Runnable() {
             @Override
             public  void run() {
                 Intent in=new Intent(SplashActivity.this,MainActivity.class);
                 startActivity(in);
-                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+
                 finish();
 
             }
