d9769dfaeeb2f2c51e917877687add82560d1e42
gwjung <gwjung@vdotdo.com>
2019-06-28 21:14:34 +0900

    password(Aes)-mainactivity(start)(19.06.28)

--- app/src/main/java/com/practice/mission1/MainActivity.java
+++ app/src/main/java/com/practice/mission1/MainActivity.java
@@ -2,7 +2,10 @@ package com.practice.mission1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.Webpage).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browser = new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.naver.com"));
+                startActivity(browser);
+            }
+        });
     }
+
+
 }
