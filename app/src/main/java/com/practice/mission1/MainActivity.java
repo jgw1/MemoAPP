6b6419f92eb8dde90083138776bbbe9eb177cf42
gwjung <gwjung@vdotdo.com>
2019-06-29 03:06:50 +0900

    password(Aes)-mainactivity(start)-Webbrowser Connect(19.06.29)

--- app/src/main/java/com/practice/mission1/MainActivity.java
+++ app/src/main/java/com/practice/mission1/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
         findViewById(R.id.Webpage).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
